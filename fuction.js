d04a8eff047e91ad6a728f577bb2133bfa020c4b
Lucia Ferrari <luferrari9@gmail.com>
2023-01-22 21:15:25 +0100

    guardar commit en el repositorio

---
+++ fuction.js
@@ -0,0 +1,7 @@
+document.getElementById ('boton').addEventListener('click', function () {
+    document.getElementById("demo").innerHTML = "liquidacion de certificados de obra, redeterminaciones de precios, polizas, recepciones provisorias, recepciones definitivas, adjudicaciones y llamados de obras";
+});
+
+document.getElementById('boton_ocultar').addEventListener('click', function () {
+    document.getElementById('demo').style.display = 'none';
+});
